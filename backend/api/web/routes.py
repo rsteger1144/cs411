7c21659023061be91d58c7dc78538953fb7218e8
xtyao <xtyao@bu.edu>
2022-11-16 22:45:35 -0500

    moved the nasa image api to the back end

--- backend/api/web/routes.py
+++ backend/api/web/routes.py
@@ -1,5 +1,8 @@
+import json
 import requests
 from . import temp
+
+from flask import request
 #from app import db
 
 def homepage_data():
@@ -18,28 +21,38 @@ def homepage_data():
 def root_site():
     return homepage_data()
 
-def getImages():
+
+# for the NASA api, only have image after 1995-06-20
+
+# example call http://localhost:5000/nasaImage?imgDate=1995-06-20
+
+def getImages(date):
     """
     Input: None
     return: a URL of the image
     """
     try:
+        
+        if date == None:
+            return "date is required"
         params = {
-            "date": "2021-05-01",
+            "date": date,
             "api_key": "5878VE4xRlytSxseglYIvUdptjap2M9wZvG07DUv"
         }
         
-        response = requests.get("GET https://api.nasa.gov/planetary/apod",params=params)
-        print(response.url)
+        response = requests.get("https://api.nasa.gov/planetary/apod",params=params)
+        # print(response.url)
         dict = {}
-        dict["url"] = response.url
-        dict["hasImage"] = True
+        dict["url"] = response.json()["url"]
+        dict["date"] = date
         return dict
     except Exception as e:
         return (str(e))
 
 
 @temp.route("/nasaImage")
-def root_site():
-    return getImages()
+def nasaImage():
+
+    return json.dumps(getImages(request.args.get("imgDate")))
+
 
