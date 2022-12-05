9f7f1f7b9548983a5660a604436b4a9b657a90fc
ebarbosa2 <ebarbosa@bu.edu>
2022-12-04 20:46:34 -0500

    Updated app.py to connect with Mongo's database

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,11 +1,9 @@
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+from flask_migrate import Migrate
 from config import config
 from flask_cors import CORS
-
-db = SQLAlchemy()
+from pymongo import MongoClient
 
 def create_app(config_name):
     """
@@ -16,11 +14,14 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     CORS(app)
+    client = MongoClient('localhost', 27017, username='CS411Group8', password='ZbjBnkmqepiweev2')
 
     from web import temp as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #db = SQLAlchemy(app)
-    #Migrate(app, db)
+    db = client.flask_db
+    todos = db.todos
+
+    Migrate(app, db)
 
     return app
